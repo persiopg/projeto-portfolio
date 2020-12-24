25ca85301f469d2c6da78bcbf2d6254e4485d6dd
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2020-12-24 00:01:08 -0300

    Initial commit

---
+++ Js/main.js
@@ -0,0 +1,20 @@
+//animaçao scroll
+$('a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),	
+			menuHeight = $('nav').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset
+	}, 500);
+});
+//animação scroll
+$('nav a').click(function(e){
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			menuHeight = $('nav').innerHeight(),
+			targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 500);
+});
